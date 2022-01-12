a7d1d1d2bae825f49edce4fbe8cfb301adad4349
Shubham Puri <86086833+shubham-puri-7742@users.noreply.github.com>
2022-01-13 00:01:50 +0530

    Added Redraw logic
    Including a check to disallow any repeat redraws (e.g. if the player enters 111)

--- PokerCPP/PokerCPP.cpp
+++ PokerCPP/PokerCPP.cpp
@@ -12,9 +12,65 @@ using namespace std;
 // Deck and hand (of 5 cards)
 Deck* deck;
 Card hand[5];
+bool flags[5];
+vector<short> selectionVec;
+
+// Redrawing cards
+bool draw() {
+    // Get the input as a string of indices to redraw
+    string input;
+    // Clear the selection vector
+    selectionVec.clear();
+    // Get the input
+    cout << "Input the #s of cards to redraw: ";
+    getline(cin, input);
+    // If nothing is entered, we are not redrawing
+    if (input.size() == 0)
+        return false;
+    // We get below this point if we are redrawing
+
+    // Redraw flags. To prevent repeat redraws
+    bool redrawFlags[5];
+    // Assume redrawing everything
+    for (short i = 0; i < 5; ++i)
+        redrawFlags[i] = true;
+
+    // For each element of the input
+    for (short i = 0; i < input.size(); ++i) {
+        // Get the input minus '0' (this gets the number of the card to redraw)
+        short n = input[i] - '0';
+        // If it is in a valid range (1-5, inclusive) AND not already redrawn
+        if (n >= 1 && n <= 5 && redrawFlags[n - 1]) {
+            // Push back the index (number - 1) and disallow the redraw flag for it
+            selectionVec.push_back(n - 1);
+            redrawFlags[n - 1] = false;
+        }
+    }
+
+    // For each element of the selection
+    for (short i = 0; i < selectionVec.size(); ++i) {
+        // Get the element of the vector
+        short j = selectionVec[i];
+        /*
+        * DEBUG
+        cout << "Redrawing " << j + 1 << endl;
+        */
+        // Deal a new card for the specified slot
+        hand[j] = deck->deal();
+        // And flag it as redrawn
+        flags[j] = true;
+    }
+
+    return true; // Because we redrew some cards
+}
 
 // The main play-game function
 void play() {
+    // Set all the redraw flags to false (default to no redraws)
+    for (short i = 0; i < 5; ++i) {
+        flags[i] = false;
+    }
+
     cout << "Pick your deck:\n1) Standard\n2) Pinochle\n";
     string deckChoice;
     getline(cin, deckChoice);
@@ -38,6 +94,22 @@ void play() {
 
     // Blank line at the end
     cout << endl;
+
+	// If redrawing
+	if (draw()) {
+		// For each of the cards in the hand
+		for (short i = 0; i < 5; ++i) {
+			// Display the card
+			cout << i + 1 << ". " << hand[i];
+			// If redrawn
+			if (flags[i]) {
+				// Add a star
+				cout << "*";
+			}
+			cout << endl; // Formatting
+		}
+		cout << endl; // More formatting
+	}
 
     // Evaluate the hand dealt
     Eval handEval(hand); // Initialise the evaluator on the hand
